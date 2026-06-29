5a55b5e5cb3231e2e0c7c2a91ace135161d4e9d4
Alexander <aburlaka@rambler.ru>
2026-06-29 16:51:02 +0300

    with emulators

--- pipelines/appium_tests.groovy
+++ pipelines/appium_tests.groovy
@@ -4,7 +4,7 @@ timeout(500) {
      currentBuild.description = "Running appium-tests on Jenkins"
 
      stage("Running appium-tests on Jenkins") {
-        sh "docker compose up -d"
+        sh "docker compose up"
         sh "docker run --shm-size=4gb --rm --network host tests_appium:1.0"
      }
    }
